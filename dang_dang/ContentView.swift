332f9010c8b7e0c5e0bb7d7745b8342b2270a53c
SPAEONKR <97925590+SPAEONkr@users.noreply.github.com>
2024-05-20 13:58:22 +0900

    Initial Commit

---
+++ dang_dang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dang_dang
+//
+//  Created by user on 2024/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dang_dang/dang_dangApp.swift
@@ -0,0 +1,17 @@
+//
+//  dang_dangApp.swift
+//  dang_dang
+//
+//  Created by user on 2024/05/20.
+//
+
+import SwiftUI
+
+@main
+struct dang_dangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
